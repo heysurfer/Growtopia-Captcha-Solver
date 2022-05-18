c81b220f2fece815b2d857ed20f24df235c93838
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-05-18 03:13:42 +0300

    Create cpp_example.cpp

---
+++ cpp_example.cpp
@@ -0,0 +1,26 @@
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+std::string GetCaptcha(std::string captchlink)
+{
+  std::string cmd="CaptchaBypasser.exe -captcha "+captchlink;
+  std::string captchaAnswer = exec(cmd.c_str());
+  if (captchaAnswer.find("Failed") != std::string::npos) 
+      std::cout << "Captcha Failed.!" << '\n';
+  else
+  {
+        std::cout << "Captcha Success.!" << '\n';
+        std::cout << captchaAnswer<< '\n';
+  }
+}
+
