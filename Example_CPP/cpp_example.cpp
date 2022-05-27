82464685e55335df2017f92eab189d620d272858
HeySurfer <58826689+heysurfer@users.noreply.github.com>
2022-05-27 20:31:38 +0300

    clean

--- Example_CPP/cpp_example.cpp
+++ Example_CPP/cpp_example.cpp
@@ -2,12 +2,9 @@
 
 std::string GetCaptcha(std::string captchaid)
 {
-    http::Request request{"http://api.surferstealer.com/captcha/index?CaptchaID"+captchaid};
-    const auto response = request.send("GET");
-
-  std::string cmd= std::string{response.body.begin(), response.body.end()}
-  std::string captchaAnswer = cmd;
-
+  http::Request request{"http://api.surferstealer.com/captcha/index?CaptchaID"+captchaid};
+  const auto response = request.send("GET");
+  std::string captchaAnswer = std::string{response.body.begin(), response.body.end()};
   if (captchaAnswer.find("Failed") != std::string::npos) 
       std::cout << "Captcha Failed.!" << '\n';
   else if (captchaAnswer.find("Answer|") != std::string::npos) 
