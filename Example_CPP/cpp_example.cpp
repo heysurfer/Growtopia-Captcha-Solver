458c32f73465f585cdb8dbc916b61ed925474c08
HeySurfer <HeySurfers@protonmail.com>
2022-09-16 23:40:48 +0300

    Update cpp_example.cpp

--- Example_CPP/cpp_example.cpp
+++ Example_CPP/cpp_example.cpp
@@ -1,17 +1,47 @@
 #include "HTTPRequest.hpp"
+#include <string>
 
-std::string GetCaptcha(std::string captchaid)
+class PuzzleSolver {
+public:
+    bool Solved = false;
+    std::string LatestAnswer = "";
+    PuzzleSolver(std::string g) : SecretKey(g) {}
+    void AddParams(std::string Key, std::string Value)
+    {
+        Params.append(Key + "=" + Value + "&");
+    }
+
+    std::string GetAnswer(std::string CaptchaUID)
+    {
+        AddParams("Action", "Solve");
+        AddParams("Puzzle", CaptchaUID);
+        AddParams("Secret", SecretKey);
+        AddParams("Format", "txt");
+        std::string FullUrl = API + Path + Params;
+        http::Request request{ std::string{FullUrl}.c_str()};
+        const auto response = request.send("GET");
+        std::string captchaAnswer = std::string{ response.body.begin(), response.body.end() };
+        Solved = captchaAnswer.find("Failed") == std::string::npos && captchaAnswer.length() > 6;
+
+        return LatestAnswer = (captchaAnswer.length() > 6) ? captchaAnswer.erase(0, 7) : "Failed";
+
+    }
+private:
+    std::string SecretKey;
+    std::string API = "http://api.surferwallet.net/";
+    std::string Path = "Captcha?";
+    std::string Params = "";
+};
+
+int main(void)
 {
-  http::Request request{"http://api.surferstealer.com/captcha/index?CaptchaID"+captchaid};
-  const auto response = request.send("GET");
-  std::string captchaAnswer = std::string{response.body.begin(), response.body.end()};
-  if (captchaAnswer.find("Failed") != std::string::npos) 
-      std::cout << "Captcha Failed.!" << '\n';
-  else if (captchaAnswer.find("Answer|") != std::string::npos) 
-  {
-        std::cout << "Captcha Success.!" << '\n';
-        std::cout << captchaAnswer<< '\n';
-        return captchaAnswer;
-  }
-    return "Fail";
+  std::string SecretCode="";
+  auto x = PuzzleSolver(SecretCode);
+  x.GetAnswer("Puzzle UID");
+  if (x.Solved)
+      std::cout << "Answer : " << x.LatestAnswer + "\n";
+  else
+      std::cout << "Failed\n";
+  
+  
 }
